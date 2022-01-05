1ab8cc78560ea587a66426b87f498d8ed11ce464
nikita <batman.spiderman2000@mail.ru>
2022-01-05 14:54:22 +0300

    second

--- script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const answers = [];
-
-answers[0] = prompt('как вас зовут?');
-answers[1] = prompt('как ваша фамилия');
-answers[2] = prompt('сколько вам лет');
-
-document.write(answers);
