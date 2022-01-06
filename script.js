b5e4a47554314d0e2d41e876c79f62df377b5611
nikita <batman.spiderman2000@mail.ru>
2022-01-06 14:37:46 +0300

    UpgradeMovieB

--- script.js
+++ script.js
@@ -10,15 +10,30 @@ const pesonalMovieDB = {
     privat: false
 };
 
-const a = prompt('какой фильм смотрел?', ''),
-      b = prompt('на скок оценишь?', ''), 
-      c = prompt('какой фильм смотрел?', ''),
-      d = prompt('на скок оценишь?', '');
+for(let i = 0; i < 2; i++){
+    const a = prompt('какой фильм смотрел?', ''),
+          b = prompt('на скок оценишь?', '');
+          
 
-pesonalMovieDB.movies[a] = b;
-pesonalMovieDB.movies[c] = d;
+          if(a != null && b != null && a!= '' && b != '' && a.length < 50  ){
+              console.log('done');
+
+          } else {
+              console.log('error');
+              i--;
+          }
+}
+         if(pesonalMovieDB.count == 10 ){
+         console.log('просмотрено довольно мало фильмов');
+
+         } else if(pesonalMovieDB.count > 10 && pesonalMovieDB.count < 30 ){
+               console.log('вы классический зритель');
+         }else if(pesonalMovieDB.count > 30){
+             console.log('вы киноман');
+         }else{
+             console.log('error');
+         }
+
 
 console.log(pesonalMovieDB);
 
-     
-     
